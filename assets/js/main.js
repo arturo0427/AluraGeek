0ce69bccfac49c92b7ea0a63c5d480a82a0d54a1
amunoz <amunoz@PASAN-TI.VFE.EC>
2023-05-17 13:10:20 -0500

    Creacion de la pantalla de login

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,9 @@ const input_search = document.querySelector(".nav__search-content");
 const btn_login = document.querySelector(".nav__button");
 
 btn_search.addEventListener("click", () => {
-  btn_login.style.display = "none";
+  if (btn_login) {
+    btn_login.style.display = "none";
+  }
   btn_search.style.display = "none";
-  input_search.style.display = "block";
+  input_search.style.display = "flex";
 });
