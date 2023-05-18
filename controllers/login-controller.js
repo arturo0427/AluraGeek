1f4ad2ca49f48a52db9efcc2f1a03ea47806e025
amunoz <amunoz@PASAN-TI.VFE.EC>
2023-05-18 12:43:01 -0500

    Pantalla productos

---
+++ controllers/login-controller.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("[data-form]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector("[data-email]").value;
+  const password = document.querySelector("[data-password]").value;
+
+  if (email.length > 5 && password.length > 5) {
+    window.location.href = "../sceens/products.html";
+  } else {
+    alert("Email y contraseña deben tener mas de 5 caracteres");
+  }
+});
